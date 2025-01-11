7c4194815470a092098cdfab64573099b3f662c2
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2025-01-11 19:14:02 +0600

    card fixed

--- syncLipika-client/src/components/EnterRoomCard/EnterRoomCard.jsx
+++ syncLipika-client/src/components/EnterRoomCard/EnterRoomCard.jsx
@@ -1,29 +1,32 @@
-
 'use client'
 import {
-    Button,
-    Card,
-    CardContent,
-    CardHeader,
-    CardTitle,
-    Divider,
-    Input,
-    InputIcon,
-    Label
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  Divider,
+  Input,
+  InputIcon,
+  Label
 } from 'keep-react'
 import { Envelope, Lock } from 'phosphor-react'
 
 export const EnterRoomCard = () => {
   return (
-    <Card className="max-w-sm">
-      <CardContent className="space-y-3">
+    <Card className="max-w-md">
+      <CardContent className="space-y-4">
         <CardHeader>
           <CardTitle>Enter the SyncSpace</CardTitle>
         </CardHeader>
 
         <Divider>Join</Divider>
-        <form className="space-y-2">
-          <fieldset className="space-y-1">
+        <form className="space-y-3">
+          <fieldset className="space-y-2">
             <Label htmlFor="email">SyncSpace ID*</Label>
             <div className="relative">
               <Input id="email" type="email" placeholder="Enter SyncSpace ID" className="ps-11" />
@@ -32,7 +35,7 @@ export const EnterRoomCard = () => {
               </InputIcon>
             </div>
           </fieldset>
-          <fieldset className="space-y-1">
+          <fieldset className="space-y-2">
             <Label htmlFor="password">User Name*</Label>
             <div className="relative">
               <Input id="password" placeholder="Enter username" type="text" className="ps-11" />
@@ -43,11 +46,26 @@ export const EnterRoomCard = () => {
           </fieldset>
           <Button 
             type="submit" 
-            className="!mt-3 block w-full ease-in-out duration-300 bg-emerald-500 text-white hover:bg-emerald-600"
+            className="!mt-4 block w-full ease-in-out duration-300 bg-emerald-500 text-white hover:bg-emerald-600"
           >
             Join Room
           </Button>
         </form>
+        <div className="text-sm flex justify-between items-center">
+          <span>Don&apos;t have a room ID?</span>
+          <div className="flex ml-1 items-center gap-1">
+            <span>Create</span>
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem>
+                  <BreadcrumbLink href="/" className="text-emerald-600 hover:underline">
+                    new room
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </div>
       </CardContent>
     </Card>
   )
