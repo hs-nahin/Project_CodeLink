0596b821c7b968e649a48d81ebff8b26f17d6415
hs-nahin <hasnatshahriyarnahin1@gmail.com>
2025-05-04 04:20:22 +0600

    temp deleted

--- codeLink-server/Temp.java
+++
@@ -1,7 +0,0 @@
-public class AddTwoNumbers {
-    public static void main(String[] args) {
-        int a = 5;
-        int b = 55;
-        System.out.println(a + b);
-    }
-}
--- codeLink-server/temp.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int a = 5;
-    int b = 55;
-    cout << a + b << " " << "eta c++ etao kaj hocche" << endl;
-    return 0;
-}
